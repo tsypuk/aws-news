3a60ee03eb64b22fec12b7f83c7830a7096f6289
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-10-15 19:03:58 +0300

    feat: update json format generation

--- aws-news-crawler/aws_news_crawler/table.py
+++ aws-news-crawler/aws_news_crawler/table.py
@@ -1,5 +1,6 @@
 import json
 
+
 def main():
     # Read the JSON file
     with open('../rss_config.json', 'r') as json_file:
@@ -10,13 +11,21 @@ def main():
 
     # Create the table content
     table_content = ""
+    table_content2 = ""
     for entry in data:
         table_content += f"| {entry['name']} | {entry['link']} |\n"
+        table_content2 += f"| {entry['name']} | https://blog.tsypuk.com/aws-news/news/{entry['name'].lower().replace(' ', '_')}.json |\n"
 
     # Write the table to a Markdown file
     with open('output.md', 'w') as md_file:
         md_file.write(table_header)
         md_file.write(table_content)
+
+    with open('output_json.md', 'w') as md_file:
+        md_file.write(table_header)
+        md_file.write(table_content2)
+
+    'https://blog.tsypuk.com/aws-news/news/architecture.json'
 
 
 if __name__ == "__main__":
