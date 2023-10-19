6a2c83e0e710657a0f3434ec3ec07c3c8e303fde
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-10-19 16:36:19 +0300

    feat: add index generation

--- aws-news-crawler/aws_news_crawler/__init__.py
+++ aws-news-crawler/aws_news_crawler/__init__.py
@@ -1,4 +1,7 @@
+import hashlib
 import json
+import os
+
 import feedparser
 
 
@@ -7,19 +10,39 @@ def main():
     with open(file_path, 'r') as file:
         data = json.load(file)
 
+    output_data = []
+
     for item in data:
         feed_url = item["link"]
         file_name = item["name"].lower().replace(" ", "_")
+        file_path = f'../news/{file_name}.json'
 
-        # Parse the RSS feed
         feed = feedparser.parse(feed_url)
 
-        # Extract titles and links
         entries = [{'title': entry.title, 'link': entry.link} for entry in feed.entries]
 
-        # Write to JSON file
         with open(f'../news/{file_name}.json', 'w') as json_file:
             json.dump(entries, json_file, indent=4)
+
+        size = os.path.getsize(file_path)
+        timestamp = os.path.getmtime(file_path)
+        with open(file_path, 'rb') as f:
+            content = f.read()
+            sha256_hash = hashlib.sha256(content).hexdigest()
+
+        file_info = {
+            'name': file_name,
+            'size': size,
+            'timestamp': timestamp,
+            'hash': sha256_hash
+        }
+
+        output_data.append(file_info)
+
+    with open('index.json', 'w') as output_file:
+        json.dump(output_data, output_file, indent=4)
+
+
 
 
 if __name__ == "__main__":
